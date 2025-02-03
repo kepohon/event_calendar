8e033128d518d94d7d077127b02c23658e869c26
kepohon <knaka278@gmail.com>
2025-02-03 21:18:55 +0900

    event_calendarの初期バージョン

--- event_calendar/__main__.py
+++ event_calendar/__main__.py
@@ -1,7 +1,11 @@
-#from .template import main
+from .event_calendar import EventCalendar
 
 if __name__ == '__main__':
     print("__main__.py")
     print("  メインプログラムの起点。")
     print("event calendar project")
+    
+    ec = EventCalendar()
+    ec.mainloop()
+
     #main()
